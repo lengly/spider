bc65eea9acb4d1ae0a61e39669bc828ef3b40f84
lengly <liu_yuda@163.com>
2015-04-19 00:25:33 +0800

    second page

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import re
 def getHtml(url):
     user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
     headers = { 'User-Agent' : user_agent}
-    values = {}
-    data = urllib.urlencode(values)
+    #values = {}
+    #data = urllib.urlencode(values)
 
-    req = urllib2.Request(url, data, headers)
+    req = urllib2.Request(url = url, headers = headers)
     try:
         response = urllib2.urlopen(req)
         return response.read()
@@ -19,28 +19,40 @@ def getHtml(url):
 
 numSt = 100000
 numEnd = 2200000
-for i in range(numSt, numEnd):
+for i in range(numSt, numSt + 20):
     url1 = 'http://www.ppdai.com/list/' + str(i) + '?loanlist'
 
     html = getHtml(url1)
 
     if (html != 'Not Found'):
+        record = []
         #获取  名字
         reg = r'class="username">(.+)</a>'
         name = re.findall(reg, html)
-        print i, name[0],
+        record.append(i)
+        record.append(name[0])
         #获取  接待总额  年利率  期限
         reg = r'<dd>(?:<em>.*</em>)?([0-9.,]+)(?: ?<em>.*</em>)?</dd>'
         list = re.findall(reg, html)
-        """
+        record.extend(list)
+
         #获取  借入信用  借出信用  性别  年龄
         url = 'http://www.ppdai.com/user/' + name[0]
-        print url
         html = getHtml(url)
-        print html
-        """
+        reg = r'class="cf7971a">([0-9]+)'
+        list = re.findall(reg, html)
+        record.extend(list)
+        reg = r'<p class="lh30 txc"><span> *(.+)</span>'
+        sex = re.findall(reg, html)
+        record.extend(sex)
+        reg = r'([^ \n]+) *</span></p>'
+        age = re.findall(reg, html)
+        record.extend(age)
 
-        print list[0], list[1], list[2]
+        for value in record:
+            print value,
+        print
+
 
 
 
